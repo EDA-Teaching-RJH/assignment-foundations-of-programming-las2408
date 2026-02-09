3603cf672dc34b4ebfb25a47ccd2d7dfc7d62e8f
las2408 <las1@kent.ac.uk>
2026-02-09 20:20:58 +0000

    line 60

--- testing.py
+++ testing.py
@@ -1,1 +1,66 @@
-+ " - " + d[i]) # displays the divisions of each memeber
+#+ " - " + d[i]) # displays the divisions of each memeber
+
+n = ["Picard", "Riker", "Data", "Worf"]
+r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+d = ["Command", "Command", "Operations", "Security"]
+
+active = True
+
+def run_system_monolith():
+    print("BOOTING SYSTEM...")
+    print("...")
+    print("WELCOME TO FLEET COMMAND")
+    
+    
+    loading = 0
+    while loading < 5:
+        print("Loading module " + str(loading))
+        loading = loading + 1 # takes us out of the loop and opens the menu
+        
+    
+    while True:
+        print("\n--- MENU ---")
+        print("1. View Crew")
+        print("2. Add Crew")
+        print("3. Remove Crew")
+        print("4. Analyze Data")
+        print("5. Exit")
+        
+        opt = input("Select option: ")
+        
+        if opt == "1":  # == instead of =
+            print("Current Crew List:")
+            
+            for i in range(len(n)): # keeps the index in the range of the list
+                print(n[i] + " - " + r[i])
+                
+        elif opt == "2":
+            new_name = input("Name: ")
+            new_rank = input("Rank: ")
+            new_div = input("Division: ")
+            
+           
+            n.append(new_name)
+            r.append(new_rank) # added crew memeber rank
+            d.append(new_div) # added crew memeber division
+            print("Crew member added.")
+            
+        elif opt == "3":
+            rem = input("Name to remove: ")
+           
+            idx = n.index(rem)
+            n.pop(idx)
+            r.pop(idx)
+            d.pop(idx)
+            print("Removed.")
+            
+        elif opt == "4":
+            print("Analyzing...")
+            count = 0
+            
+            for rank in r:
+                if rank == "Captain" or "Commander": 
+                    count = count + 1
+            print(f"High ranking officers: " + {count}) 
+
+run_system_monolith()
